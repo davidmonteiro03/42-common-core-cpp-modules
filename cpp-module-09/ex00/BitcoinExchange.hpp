b8ed4e3e2bdfdffb1561f5a56bbdbb6f93b44038
davidmonteiro03 <damonteiro12446@gmail.com>
2025-04-11 16:59:47 +0100

    Remaking cpps to a better learning experience - cpp09-ex00 almost done

--- cpp-module-09/ex00/BitcoinExchange.hpp
+++ cpp-module-09/ex00/BitcoinExchange.hpp
@@ -5,65 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 10:14:21 by dcaetano          #+#    #+#             */
-/*   Updated: 2025/02/16 09:51:24 by dcaetano         ###   ########.fr       */
+/*   Created: 2025/04/11 14:42:05 by dcaetano          #+#    #+#             */
+/*   Updated: 2025/04/11 14:45:47 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <cstdlib>
-#include <cstring>
-#include <exception>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <sstream>
-
-#define COULD_NOT_OPEN_FILE "Error: could not open file."
-#define INVALID_DATABASE_CSV "Error: invalid database csv."
-#define INVALID_INPUT_FILE "Error: invalid input file."
-#define DATE_IS_TOO_OLD "Error: date is too old"
-
-typedef struct s_date
-{
-	unsigned int year, month, day;
-} t_date;
-
 class BitcoinExchange
 {
-private:
-	std::map<std::string, double> _database;
-	void _readCSV(std::string);
-	void _readInput(std::string);
-	t_date _createDate(std::string, std::string, std::string);
-	unsigned int _getNumDays(t_date);
-	std::string _sDate(t_date);
-	double _searchExchange(t_date);
-	class CouldNotOpenFile : public std::exception
-	{
-	public:
-		virtual const char *what() const throw();
-	};
-	class InvalidDatabaseCSV : public std::exception
-	{
-	public:
-		virtual const char *what() const throw();
-	};
-	class InvalidInputFile : public std::exception
-	{
-	public:
-		virtual const char *what() const throw();
-	};
-	class DateIsTooOld : public std::exception
-	{
-	public:
-		virtual const char *what() const throw();
-	};
-
 public:
 	BitcoinExchange(void);
+	BitcoinExchange(const BitcoinExchange &);
+	BitcoinExchange &operator=(const BitcoinExchange &);
 	~BitcoinExchange();
-	void _execute(std::string);
 };
